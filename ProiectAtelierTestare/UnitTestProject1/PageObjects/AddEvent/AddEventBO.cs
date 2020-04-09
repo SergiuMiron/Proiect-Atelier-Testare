81219183d5a72a169622c5c41d5e4da2a1f9e9c0
Marius Gutu <cociurcapatric@gmail.com>
2020-04-09 19:45:58 +0300

    EVENT and IDP tests

---
+++ ProiectAtelierTestare/UnitTestProject1/PageObjects/AddEvent/AddEventBO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.PageObjects.AddEvent
+{
+    public class AddEventBO
+    {
+        public string Name { get; set; } = "Test Name";
+        public string Date = "";
+        public string Participant = "Mike Mathews";
+        public string Owner = "Odis Adalwin";
+        public string Company = "Test Company";
+        public string Duration = "12:07";
+        public string Description = "Test Description";
+    }
+}
